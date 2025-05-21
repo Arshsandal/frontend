ff2669189bf7a7780e2ef30c337ae27503c2f754
Arshsandal <arshsandal50@gmail.com>
2025-05-21 13:53:42 +0530

    Fix Navbar Path of the logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import logo from "../assets/Images/Logo_White.svg"
+import logo from "../assets/Images/Logo_white.svg"
 
 const Navbar = () => {
   const [username, setUsername] = useState("");
